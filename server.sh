3c32c2e36c42ef79cab32ca7776c0852b163f688
Elliot Tester <166047473+SuperbMuffin@users.noreply.github.com>
2025-11-28 18:57:55 +0100

    server.sh to create a hhostettp server

---
+++ server.sh
@@ -0,0 +1,12 @@
+  GNU nano 8.7                      server.sh
+#!/usr/bin/env bash
+
+SHARE_DIR="$home/.recall/shared"
+mkdir -p "$SHARE_DIR"
+
+PORT=8080
+
+echo "Hosting fun Collaberation Feature on http://$(hostname -s):$PORT/"
+
+# Start BusyBox HTTP server
+busybox httpd -f -p "$PORT" -h "$SHARE_DIR"
